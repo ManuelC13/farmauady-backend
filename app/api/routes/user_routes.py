894a4b5e65afc658080c5c630f79d7de1fe0d4a1
Manuel Cupul <114542854+ManuelC13@users.noreply.github.com>
2026-04-06 12:47:02 -0600

    feat: add access control to user routes

--- app/api/routes/user_routes.py
+++ app/api/routes/user_routes.py
@@ -4,13 +4,12 @@ from sqlalchemy.orm import Session
 from app.db.database import get_db
 from app.schemas.user import UserCreate, UserResponse, UserUpdate
 from app.services import user_service
-
+from app.services.auth.auth_service import get_current_user, RoleChecker
 from typing import List
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-
-@router.post("/", response_model=UserResponse)
+@router.post("/", response_model=UserResponse, dependencies=[Depends(RoleChecker(["Administrador"]))])
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     try:
         return user_service.create_user(db, user)
@@ -18,37 +17,44 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail=str(e))
 
 
-@router.get("/", response_model=List[UserResponse])
+@router.get("/", response_model=List[UserResponse], dependencies=[Depends(RoleChecker(["Administrador"]))])
 def list_users(db: Session = Depends(get_db)):
     return user_service.get_users(db)
 
 
 @router.get("/{user_id}", response_model=UserResponse)
-def get_user(user_id: int, db: Session = Depends(get_db)):
+def get_user(
+    user_id: int,
+    db: Session = Depends(get_db),
+    current_user = Depends(get_current_user)
+):
     user = user_service.get_user_by_id(db, user_id)
-    
+
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+    if current_user.role.name != "Administrador" and current_user.id_user != user_id:
+        raise HTTPException(status_code=403, detail="No autorizado") # Si no es su propio perfil ni es admin, no puede verlo
+
     return user
 
 
-@router.put("/{user_id}", response_model=UserResponse)
+@router.put("/{user_id}", response_model=UserResponse, dependencies=[Depends(RoleChecker(["Administrador"]))])
 def update_user(user_id: int, updates: UserUpdate, db: Session = Depends(get_db)):
     user = user_service.get_user_by_id(db, user_id)
 
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
 
     return user_service.update_user(db, user, updates)
 
 
-@router.delete("/{user_id}")
+@router.delete("/{user_id}", dependencies=[Depends(RoleChecker(["Administrador"]))])
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     user = user_service.get_user_by_id(db, user_id)
 
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
 
     user_service.delete_user(db, user)
     return {"message": "Usuario eliminado exitosamente"}
